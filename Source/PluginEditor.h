f8e8e19410448956f13d017e86e53fa6445fb5b6
BoscoDA <nickwe02@gmail.com>
2023-01-05 19:26:53 -0600

    Customized toggle button graphics

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -177,6 +177,11 @@ struct LookAndFeel : juce::LookAndFeel_V4
         float rotaryStartAngle,
         float rotaryEndAngle,
         juce::Slider&) override;
+    void drawToggleButton(
+        juce::Graphics& g,
+        juce::ToggleButton& toggleButton,
+        bool shouldDrawButtonAsHighlighted,
+        bool shouldDrawButtonAsDown) override;
 };
 
 struct RotarySliderWithLabels : juce::Slider
@@ -318,8 +323,9 @@ private:
         highcutBypassButtonAttachment,
         analyzerEnableButtonAttachment;
 
-
     std::vector<juce::Component*> getComps();
+
+    LookAndFeel lnf;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
